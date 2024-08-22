2ba6a04f1f234a9ac0fc8b5b6ef6316c49c62493
PrincyChauhan <chauhanprincee7@gmail.com>
2024-08-22 10:29:38 +0530

    factorial number

--- factorial.py
+++ factorial.py
@@ -1,10 +1,22 @@
-n=int(input("Enter a number: "))
+# n=int(input("Enter a number: "))
 
-def fact(n):
+# def fact(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n * fact(n-1)
+
+# result=fact(n)
+# print(result)
+
+def factorial(n):
     if n==0:
         return 1
+    elif n<0:
+        return -1
     else:
-        return n * fact(n-1)
-
-result=fact(n)
-print(result)
+        return n * factorial(n-1)
+    
+print((factorial(5))) 
+print((factorial(0)))
+print((factorial(-5)))   
