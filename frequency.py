5eb010b117c5b1e0205002228e46f28168d7b716
PrincyChauhan <chauhanprincee7@gmail.com>
2024-08-31 15:23:27 +0530

    Find Frequency of an element in an Array

---
+++ frequency.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+# Example array
+array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+
+# Create a Counter object
+frequency = Counter(array)
+
+# Print the frequencies
+print(frequency)
