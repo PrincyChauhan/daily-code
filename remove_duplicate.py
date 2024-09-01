1eef10912c61e7c368b412d63e45f172cf72c771
PrincyChauhan <chauhanprincee7@gmail.com>
2024-09-01 18:53:43 +0530

    Remove Duplicate from an Unsorted Array

--- remove_duplicate.py
+++ remove_duplicate.py
@@ -1,5 +1,15 @@
-arr=[1,1,2,2,2,3,3]
-my_set=set(arr)
-s = list(my_set)
-a=len(s)
-print(a)
+# arr=[1,1,2,2,2,3,3]
+# my_set=set(arr)
+# s = list(my_set)
+# a=len(s)
+# print(a)
+
+def solution(arr,N):
+    unique=[]
+    for i in range(N):
+        if arr[i] not in unique:
+            unique.append(arr[i])
+    print(unique)  
+arr=[4,4,1,1,3,3,2,2,2]
+N=len(arr)
+solution(arr,N)          
