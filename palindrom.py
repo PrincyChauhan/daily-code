19abb4730c48e1726fe11efd1c8376cf68729bb1
PrincyChauhan <chauhanprincee7@gmail.com>
2024-09-03 10:07:02 +0530

    Palindrom string

--- palindrom.py
+++ palindrom.py
@@ -1,7 +1,20 @@
-stri = "TAKE U FORWARD"
+# stri = "TAKE U FORWARD"
 
-rev_str=stri[::-1]
-if stri == rev_str:
-    print("It is a palindrome")
-else:
-    print("It is not a palindrome")    
+# rev_str=stri[::-1]
+# if stri == rev_str:
+#     print("It is a palindrome")
+# else:
+#     print("It is not a palindrome")    
+
+
+def isPalindrom(s):
+    start,end=0,len(s)-1
+    while start<=end:
+        if s[start]!=s[end]:
+            return "Not a Palindrom"
+        start+=1
+        end-=1
+    return "It is a Palindrom"
+
+given_string=input()
+print(isPalindrom(given_string))
