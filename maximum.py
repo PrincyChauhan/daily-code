8b51d724e754655e2efbe3f8d7f478eb613b2a61
PrincyChauhan <chauhanprincee7@gmail.com>
2024-09-26 14:08:48 +0530

    find the maximum element in each of contigous subarray

---
+++ maximum.py
@@ -0,0 +1,23 @@
+# Given an array and a integer K
+# we need to find the maximum element in each of contigous subarry
+# input 2 4 7 1 6 3
+# k=3
+# output 7 7 7 6
+
+
+arr=list(map(int,input().split()))
+k=int(input())
+
+n=len(arr)
+
+res=[]
+
+for i in range(n-k+1):
+    subarr=arr[i:i+k]
+    print(subarr,"----subarr------")
+    
+    max_val=max(subarr)
+    print(max_val,"----max_val------")
+    
+    res.append(max_val)
+print(res,"----res------")
